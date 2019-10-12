4f44106e4c4f2a6dc0e007a5a5529ecfd3360fb3
Adric Sutton <suttonadric@gmail.com>
2019-10-12 08:57:43 -0600

    added htmlunit and able to read webpage from scraper refer to WebCrawler.iml for where to extract htmlunit to

---
+++ src/com/company/Scrape.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+
+public class Scrape {
+    public static void main(String[] args) throws IOException {
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        HtmlPage page = webClient.getPage("https://wklyads.kingsoopers.com/flyers/kingsoopers-weekly/grid_view/417480?postal_code=80012&store_code=00049&type=2");
+        webClient.getOptions().setJavaScriptEnabled(true);
+        String pageContent=page.asXml();
+        System.out.println(pageContent);
+
+    }
+}
